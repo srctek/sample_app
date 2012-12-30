d2aa83d0587a41715f7173d6ea7670d9ac28075a
srctek <info@srctek.com>
2012-12-30 20:31:58 +0800

    Finish sign in

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -53,6 +53,6 @@ describe "Static pages" do
     click_link "Sign up now!"
     page.should have_selector 'title', text: full_title('Sign up')
     click_link "sample app"
-    page.should have_selector 'title', text: full_title('Sign up')
+    page.should have_selector 'title', text: full_title('')
   end  
 end
